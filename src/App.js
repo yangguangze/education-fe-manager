9407ddb16b06dcb3045e9f509e5e7f431e6465a3
shenglong.li <913937000@qq.com>
2017-12-21 10:47:37 +0800

    change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          teaching program
+          1720 gift
         </p>
       </div>
     );
